11ca8096a7a58a00ab9b09ba0bfac29f904a5a0d
juzhu2 <justinzhu1224@gmai.com>
2025-12-28 22:27:50 -0800

    added constant

---
+++ gameconstant.py
@@ -0,0 +1,43 @@
+# Game Constants for starting parameters
+STARTING_HEALTH = 5
+MAX_HEALTH = 10
+
+# Hand size constraints
+HAND_SIZE = 3
+MAX_HAND_SIZE = 7
+
+#Board size constraints
+INITIAL_BOARD_SIZE = 3
+MAX_BOARD_SIZE = 9
+
+# Win condition constraints
+INITIAL_WIN_CONDITION_LENGTH = 3
+MAX_WIN_CONDITION_LENGTH = 5
+
+# Damage per N in a row constraints
+INITIAL_DAMAGE_PER_N_IN_ROW = 1
+MAX_DAMAGE_PER_N_IN_ROW = 2
+
+# Cards played per turn constraints
+INITIAL_CARDS_PLAYED_PER_TURN = 1
+MAX_CARDS_PLAYED_PER_TURN = 2
+
+# Overtime mode parameters
+OVERTIME_TRIGGER_TURN = 30 # Turn number when overtime starts
+OVERTIME_DAMAGE_MULTIPLIER = 2 # Damage multiplier during overtime
+OVERTIME_EXTRA_CARDS_PER_TURN = 1 # Extra cards allowed to play per turn during overtime
+
+# Mana constraints
+INITIAL_MANA = 3
+MAX_MANA = 10
+MANA_REGEN_PER_TURN = 1
+MAX_MANA_REGEN_PER_TURN = 2
+
+# Card draw constraints
+INITIAL_CARDS_DRAWN_PER_TURN = 1
+MAX_CARDS_DRAWN_PER_TURN = 2
+
+#Symbolic constants
+EMPTY_CELL = '.'
+PLAYER_X_SYMBOL = 'X'
+PLAYER_O_SYMBOL = 'O'
